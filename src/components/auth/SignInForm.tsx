2600114d1d458de25e0a0d8ad6328355b6bbf7db
Lukman Azri <mlukman.azri@gmail.com>
2025-05-29 11:49:54 +0800

    fix

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from '../../context/AuthContext';
 
 export default function SignInForm() {
